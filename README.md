ea48d1bca9b008dec0b4f2d5d201ec7b9f3b5ebd
Eslam ElHusseiny <eslam.husseiny@gmail.com>
2018-01-03 01:07:19 +0000

    Initial test

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # CNA Tutorial
+VirtualBox => Preferences => Network => Host-only Networks
+![VBox network settings 1](https://github.com/EslamElHusseiny/CNA_tutorial/blob/master/vbox1.png "VBox Network Settings 1")
+
+Create a new Network with IP => 192.168.58.1 and Network Mask => 255.255.255.0
+![VBox network settings 2](https://github.com/EslamElHusseiny/CNA_tutorial/blob/master/vobx2.png "VBox Network Settings 2")
+
+| Component     | IP           | Port  |
+| ------------- |------------  | ----- |
+| Docker Host   | 192.168.58.2 |       |
+| Portainer     | 192.168.58.3 | 9000  |
+| registry-srv  | 192.168.58.4 | 5000  |
+| registry-gui  | 192.168.58.5 | 8080  |
+| minio-target  | 192.168.58.6 | 9000  |
+| gitlab        | 192.168.58.7 | 30080 |
+| concourse-web | 192.168.58.8 | 9004  |
--- setupVagrant.sh
+++ setupVagrant.sh
@@ -2,8 +2,15 @@
 echo "Provisioning virtual machine..."
 apt-get update
 apt-get install -y virtualbox-guest-additions-iso
+apt-get install -y bridge-utils
 echo "Kill all Docker Containers"
 docker kill $(docker ps -q)
+
+## Create shared docker network
+ip addr del 192.168.58.2/24 dev eth1
+docker network create --driver bridge --subnet=192.168.58.0/24 --gateway=192.168.58.2 --opt "com.docker.network.bridge.name"="docker1" shared_nw
+brctl addif docker1 eth1
+
 # installing PCF CLI
 if [ -f "/vagrant/cf-cli-installer_6.30.0_x86-64.deb" ]
 then
@@ -18,7 +25,7 @@ mv fly_linux_amd64 fly
 cp fly /usr/local/bin/fly
 chmod 0755 /usr/local/bin/fly
 echo "Starting Portainer"
-docker run --name portainer -d -p 9002:9000 --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer:/data portainer/portainer
+docker run --name portainer --net shared_nw --ip 192.168.58.3 -d --restart always -v /var/run/docker.sock:/var/run/docker.sock -v /opt/portainer:/data portainer/portainer
 echo "Configuring Concourse CI"
 mkdir -p /vagrant/concourse_ci/keys/web /vagrant/concourse_ci/keys/worker
 ssh-keygen -t rsa -f /vagrant/concourse_ci/keys/web/tsa_host_key -N ''
@@ -31,15 +38,15 @@ export CONCOURSE_EXTERNAL_URL=http://127.0.0.1:9004
 docker-compose -f /vagrant/concourse_ci/docker-compose.yml up -d
 echo "Starting Registry"
 # docker-compose?
-docker run -d -p 5000:5000 --name registry-srv registry:2
-docker run -d -p 9001:8080 --name registry-gui --link registry-srv -e REGISTRY_URL=http://registry-srv:5000/v2 -e REGISTRY_NAME=localhost:5000 hyper/docker-registry-web 
+docker run -d --net shared_nw --ip 192.168.58.4 --name registry-srv registry:2
+docker run -d --net shared_nw --ip 192.168.58.5 --name registry-gui --link registry-srv -e REGISTRY_URL=http://registry-srv:5000/v2 -e REGISTRY_NAME=localhost:5000 hyper/docker-registry-web 
 echo "Starting Minio"
 export MINIO_DOCKER_NAME=minio-target
 export MINIO_ACCESS_KEY=admin
 export MINIO_SECRET_KEY=Password1!
 export MINIO_BUCKET=releases
-docker run --name $MINIO_DOCKER_NAME -d -p 9000:9000 -e MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY -e MINIO_SECRET_KEY=$MINIO_SECRET_KEY minio/minio server /data
-docker run --rm --link $MINIO_DOCKER_NAME:minio -e MINIO_BUCKET=$MINIO_BUCKET --entrypoint sh minio/mc -c "\
+docker run --name $MINIO_DOCKER_NAME -d --net shared_nw --ip 192.168.58.6 -e MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY -e MINIO_SECRET_KEY=$MINIO_SECRET_KEY minio/minio server /data
+docker run --rm --link $MINIO_DOCKER_NAME:minio --net shared_nw --ip 192.168.58.11 -e MINIO_BUCKET=$MINIO_BUCKET --entrypoint sh minio/mc -c "\
   while ! nc -z minio 9000; do echo 'Wait minio to startup...' && sleep 0.1; done; \
   sleep 5 && \
   mc config host add myminio http://minio:9000 \$MINIO_ENV_MINIO_ACCESS_KEY \$MINIO_ENV_MINIO_SECRET_KEY && \
@@ -50,6 +57,8 @@ docker run --rm --link $MINIO_DOCKER_NAME:minio -e MINIO_BUCKET=$MINIO_BUCKET --
 echo "Starting Gitlab"
 docker run --detach --name gitlab \
 	--hostname gitlab.example.com \
+  --net shared_nw \
+  --ip 192.168.58.7 \
 	--publish 30080:30080 \
 	--publish 30022:22 \
 	--env GITLAB_OMNIBUS_CONFIG="external_url 'http://127.0.0.1:30080'; gitlab_rails['gitlab_shell_ssh_port']=30022;" \
